c4e7f79773ef6cda938097ab0f3d381d54d09be4
Alberto Sánchez <slalbertojesus@gmail.com>
2019-09-09 00:11:28 -0700

    Se añadió pagina de inicio sesión y botón para registro

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main (){
+void main() {
   runApp(merixo());
 }
 
-class merixo extends StatelessWidget{
- @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('merixo'),),
-      body: Text('default text'),
-    ),);
+const PrimaryColor = const Color(0xFF151026);
+const SecundaryColor = const Color(0xAF0404);
+
+class merixo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: SecundaryColor,
+          appBar: AppBar(
+            backgroundColor: SecundaryColor,
+            title: Text('merixo'),
+          ),
+          body: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Entra tu teléfono'),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Entra tu contraseña'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  /*...*/
+                },
+                child: Text(
+                  "Ingresa",
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
