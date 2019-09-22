00a0870c85ccd8511341a2b1523dd5180c6ab783
Alberto Sánchez <slalbertojesus@gmail.com>
2019-09-21 23:10:37 -0700

    Se añadió estilo para caso de uso inicio de sesión

--- lib/inicio_sesion.dart
+++ lib/inicio_sesion.dart
@@ -5,28 +5,66 @@ class InicioSesion extends StatefulWidget {
 
   @override
   _InicioSesionState createState() => new _InicioSesionState();
-
 }
 
 class _InicioSesionState extends State<InicioSesion> {
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: _buildList(),
-    );
-  }
-  
-  Widget _buildList() {
-    TextField textFieldNombre =_buildTextField("Entra tu nombre");
-    return textFieldNombre;
+        body: Wrap(
+            runSpacing: 20, // to apply margin vertically
+            children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Center(
+              child: Text('merixo',
+                  style: TextStyle(fontFamily: 'GalanoGrotesque', fontSize: 32)),
+            ),
+          ),
+          _buildTextField("Usuario"),
+          _buildTextField("Contraseña"), 
+          Center(
+            child: Column (
+               children: <Widget>[
+            _buildButton("Ingresa"),
+            _buildButton("Registrate"),
+               ],
+            ),
+          )
+        ]));
   }
 
-  TextField _buildTextField(String textFieldMotive) {
-    return TextField(
-              decoration: InputDecoration(labelText: textFieldMotive),
-            );
+  FlatButton _buildButton(String buttonIdentifier){
+  FlatButton button = new FlatButton(
+                 child: Text(
+                   buttonIdentifier),
+                   onPressed: () {
+                     //
+                     //
+                   }, 
+                   color: Color.fromRGBO(253, 23, 23, 1),
+                );
+                return button;;
+  }
+
+TextField _buildTextField(String textFieldMotive) {
+    TextField texto = new TextField(
+                      decoration: new InputDecoration(
+                        labelText: textFieldMotive,
+                        fillColor: Colors.black,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(
+                          ),
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      style: new TextStyle(
+                        fontFamily: "Poppins",
+                      ),
+      //decoration: InputDecoration(labelText: textFieldMotive),
+    );
+  return texto;
   }
 
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,33 @@ void main() {
 }
 
 const PrimaryColor = Color.fromRGBO(0, 0, 0, 1);
-const SecundaryColor = Color.fromRGBO(255, 0, 0, 1); 
+const SecundaryColor = Color.fromRGBO(255, 0, 0, 1);
+const Red = Color.fromRGBO(253, 23, 23, 1);
+  
 
 class Merixo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Merixo',
-      theme: new ThemeData(
-        primaryColor: PrimaryColor,
-        primaryColorBrightness: Brightness.dark,
+    return MaterialApp(
+      title: "Merixp",
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+        primaryColor: Red,
+        accentColor: Red,
+
+        // Define the default font family.
+        fontFamily: 'Montserrat',
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
       ),
-      home: new InicioSesion(),
+      home: new InicioSesion()
     );
   }
-}
+  }
